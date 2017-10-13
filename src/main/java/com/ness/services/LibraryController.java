208cb285e20cc8cb74a96da2933c1b13d88274eb
Piyush Vaishnav <piyush.vaishnav@ness.com>
2017-10-13 12:35:45 +0530

    initial fixes to get it working

--- src/main/java/com/ness/services/LibraryController.java
+++ src/main/java/com/ness/services/LibraryController.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ness.services.model.BookBO;
-import com.ness.services.todo.TodoNotFoundException;
 import com.ness.services.service.LibraryService;
 
 @RestController
